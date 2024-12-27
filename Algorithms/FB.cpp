c8f0165fd8ca4993b588ac6aec12f49e557276c9
Eiad-Alaa <eiad.alaa.nouh@gmail.com>
2024-12-28 01:10:30 +0200

    Fix Feedback algorithm quantum problem

--- Algorithms/FB.cpp
+++ Algorithms/FB.cpp
@@ -6,7 +6,8 @@ using namespace std;
 task fb(task t)
 {
   bool fb1 = false;
-  if(t.policy[0]=='6')fb1 = 1;
+  if (t.policy[0] == '6')
+    fb1 = 1;
   int time = -1;
   vector<vector<int>> time_p(t.len + 3);
 
@@ -15,7 +16,7 @@ task fb(task t)
     time_p[p.arrival].push_back(p.idx);
   }
 
-  vector<deque<process>> ready_run(t.len+2);
+  vector<deque<process>> ready_run(t.len + 2);
 
   while (time <= t.len)
   {
@@ -25,8 +26,8 @@ task fb(task t)
       process temp = ready_run[queue_id].front();
       ready_run[queue_id].pop_front();
 
-      int rest = fb1?1:(1 << queue_id);
-      
+      int rest = fb1 ? 1 : (1 << queue_id);
+
       while (temp.rem != 0 && rest != 0)
       {
         rest--;
@@ -37,24 +38,18 @@ task fb(task t)
         {
           ready_run[0].push_back(t.processes[id]);
         }
-        if (rest > 1 && !ready_run[0].empty())        
-            break;
       }
       if (temp.rem == 0)
       {
         temp.finish = time;
         t.processes[temp.idx] = temp;
       }
-      else if (rest == 0)
+      else
       {
         if (first_nempty(ready_run) != -1)
           ready_run[queue_id + 1].push_back(temp);
         else
           ready_run[queue_id].push_back(temp);
-      }
-      else
-      {
-        ready_run[queue_id].push_front(temp);
       }
     }
     else
